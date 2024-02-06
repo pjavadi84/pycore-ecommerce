092cc1d3bb388340a74f381eb33fd5faa48d76b1
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2024-02-05 21:46:11 -0800

    fixed import to serve index from the correct path. server render fixed but require to migrate. will migrate once my models are setup

--- pycore/settings.py
+++ pycore/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,8 +54,9 @@ ROOT_URLCONF = 'pycore.urls'
 
 TEMPLATES = [
     {
+        # 'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'static/frontend'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
