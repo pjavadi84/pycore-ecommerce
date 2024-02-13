4719185fc357d5b675ae0fc00108f99cec856c86
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2024-02-12 22:19:01 -0800

    Added CartItem's model

--- pycoreshop/models.py
+++ pycoreshop/models.py
@@ -47,3 +47,12 @@ class Cart(models.Model):
 
     def __str__(self):
         return f"Cart {self.id}"
+    
+#4) CartItem
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
