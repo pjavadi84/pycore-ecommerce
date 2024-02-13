276322e8bd9f280e99d49677b18e54a0e4acc6ee
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2024-02-12 22:15:10 -0800

    add product model

--- pycoreshop/models.py
+++ pycoreshop/models.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.db import models
 from django.urls import reverse
 
+# 1) Category
 class Category(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
@@ -19,3 +20,21 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return reverse('category_detail', args=[self.slug])
+    
+#2 Product
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.slug])
