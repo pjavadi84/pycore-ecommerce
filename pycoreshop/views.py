e966d5919c617ffdd809ca0dab23ac0344a71143
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2024-02-12 23:50:43 -0800

    added product list view

--- pycoreshop/views.py
+++ pycoreshop/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from .models import Category, Product
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 def index(request):
     return render(request, 'frontend/index.html')
+
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    
+    return render(request, 'product/list.html', {'category': category, 'categories': categories, 'products': products})
+
