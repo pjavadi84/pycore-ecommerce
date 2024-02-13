292e632a5d7a281637d5a05a9cae1521b8159a27
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2024-02-12 22:17:15 -0800

    added Cart model

--- pycoreshop/models.py
+++ pycoreshop/models.py
@@ -21,7 +21,7 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('category_detail', args=[self.slug])
     
-#2 Product
+#2) Product
 class Product(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
@@ -38,3 +38,12 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('product_detail', args=[self.slug])
+    
+# 3) Cart
+class Cart(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Cart {self.id}"
