4adfe6115d0f7e892eee1f20dea1d56e0ccf9d82
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2024-02-12 22:26:38 -0800

    added OrderItem's model

--- pycoreshop/models.py
+++ pycoreshop/models.py
@@ -69,3 +69,12 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order {self.id}"
+# 6) OrderItem
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
