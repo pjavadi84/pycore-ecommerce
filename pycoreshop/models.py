a21d4ddd0c145319efd4980c8bbe8f23fe84131b
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2024-02-12 21:35:06 -0800

    created category model

--- pycoreshop/models.py
+++ pycoreshop/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+from django.conf import settings
+from django.db import models
+from django.urls import reverse
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    class Meta:
+        unique_together = ('slug', 'parent',)
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('category_detail', args=[self.slug])
